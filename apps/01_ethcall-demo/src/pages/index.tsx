d6a169a0e78c00392d02ab2c314f59548710c5d6
ginlink <ginlink1998@gmail.com>
2022-07-23 21:31:08 +0800

    style: remove unused in 01

--- apps/01_ethcall-demo/src/pages/index.tsx
+++ apps/01_ethcall-demo/src/pages/index.tsx
@@ -1,19 +1,9 @@
 import React from 'react'
-import { Box } from '@ginlink/uilib'
-import { Hello } from '@/components/Hello'
-import { GradientTextDemo } from '@/components/GradientText'
-// import { SentryTest, TestResponsive } from '@/components/Test'
 
 export default function index() {
   return (
-    <Box m={3}>
-      <Hello />
-
-      <GradientTextDemo />
-
-      {/* <SentryTest /> */}
-
-      {/* <TestResponsive /> */}
-    </Box>
+    <div>
+      <h1>Hello</h1>
+    </div>
   )
 }
